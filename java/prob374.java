edb662aaedaaf26be8bb1086c5e81352cc834347
Suchetha <83269185+SSuchetha@users.noreply.github.com>
2022-04-06 20:07:40 +0530

    Create prob374.java

---
+++ java/prob374.java
@@ -0,0 +1,40 @@
+/*374. Guess Number Higher or Lower
+We are playing the Guess Game. The game is as follows:
+I pick a number from 1 to n. You have to guess which number I picked.
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+You call a pre-defined API int guess(int num), which returns three possible results:
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+Example 1:
+Input: n = 10, pick = 6
+Output: 6*/
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) 
+    {
+        int left = 1;
+        int right = n;
+        while(left < right)
+        {
+            int mid = left + (right -left)/2;
+            int res = guess(mid);
+            if(res == 0) return mid;
+            else if( res > 0)
+                left = mid+1;
+            else
+                right = mid-1;
+        }
+        return left;
+    }
+}
