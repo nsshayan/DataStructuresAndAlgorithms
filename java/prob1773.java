f449d9c274647c8a959066c2a3aeb8c7166535eb
Suchetha <83269185+SSuchetha@users.noreply.github.com>
2022-04-21 23:01:46 +0530

    Create prob1773.java

---
+++ java/prob1773.java
@@ -0,0 +1,38 @@
+/*
+1773. Count Items Matching a Rule
+
+You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule 
+represented by two strings, ruleKey and ruleValue.
+
+The ith item is said to match the rule if one of the following is true:
+
+ruleKey == "type" and ruleValue == typei.
+ruleKey == "color" and ruleValue == colori.
+ruleKey == "name" and ruleValue == namei.
+
+Return the number of items that match the given rule.
+
+Example 1:
+
+Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+Output: 1
+Explanation: There is only one item matching the given rule, which is ["computer","silver","lenovo"].
+*/
+
+class Solution 
+{
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) 
+    {
+        int count=0;
+        for(List<String> list : items) 
+        {
+            if(ruleKey.equals("type") && ruleValue.equals(list.get(0))) 
+                count++;
+            else if(ruleKey.equals("color") && ruleValue.equals(list.get(1))) 
+                count++;
+            else if(ruleKey.equals("name") && ruleValue.equals(list.get(2))) 
+                count++;
+        }
+        return count;
+    }
+}
