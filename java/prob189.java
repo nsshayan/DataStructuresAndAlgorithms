bc7ac28b461e5078b1fa44eff65024e7e5b12c2e
Suchetha <83269185+SSuchetha@users.noreply.github.com>
2022-09-03 12:47:42 +0530

    Create prob189.java

---
+++ java/prob189.java
@@ -0,0 +1,24 @@
+/*189. Rotate Array
+
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]*/
+
+class Solution 
+{
+    public void rotate(int[] nums, int k) 
+    {
+        int [] temp=new int[nums.length];
+        for(int i=0; i<nums.length; i++)
+            temp[(i+k)%nums.length]=nums[i];
+        for(int i=0; i<temp.length; i++)
+            nums[i]=temp[i];
+    }
+}
