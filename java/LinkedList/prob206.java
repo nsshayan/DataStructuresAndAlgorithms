2711f71c98bd2e7df1d2cfdfb13dc0ee37746016
Suchetha <83269185+SSuchetha@users.noreply.github.com>
2022-09-08 22:13:33 +0530

    Create prob206.java

---
+++ java/LinkedList/prob206.java
@@ -0,0 +1,34 @@
+/*
+206. Reverse Linked List
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution 
+{
+    public ListNode reverseList(ListNode head) 
+    {
+        ListNode prev=null,cur=head;
+        while(cur!=null)
+        {
+            ListNode nxt=cur.next;
+            cur.next=prev;
+            prev=cur;
+            cur=nxt;
+        }
+        return prev;
+    }
+}
